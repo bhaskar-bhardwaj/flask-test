237f434a4b4f4ab56c3569eaf7377aac94aaabe3
bhaskar <bhaskar15435@gmail.com>
2021-05-31 19:54:06 +0530

    fix

--- hello.py
+++ hello.py
@@ -1,14 +1,6 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/admin')
+@app.route('/')
 def hello_world():
    return "Hello World"
-
-@app.post('/fxn')
-def test_fxn():
-    return "Just test fxn"
-
-if __name__ == '__main__':
-   app.debug = True
-   app.run()
